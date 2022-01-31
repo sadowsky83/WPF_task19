c5944312181f42a640c7ceb0207a0dc7d338e8ef
Anatoly Sadowsky <sadowsky83@gmail.com>
2022-01-31 21:42:54 +0700

    Добавьте файлы проекта.

---
+++ Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_task19.Models
+{
+    static class Ariph
+    {
+        public static double Add(double r) => 2 * Math.PI * r;
+    }
+}
